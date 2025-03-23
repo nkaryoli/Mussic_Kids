98cf897f1456d0be24297b4310516c256dbceb01
karyoli.ie@gmail.com <148868767+nkaryoli@users.noreply.github.com>
2025-03-23 11:44:23 +0100

    accessible navbar

--- src/javascript/navbar.js
+++ src/javascript/navbar.js
@@ -1,9 +1,30 @@
 const navbar = document.getElementById('navbar');
+const openButton = document.getElementById('open-btn');
+
+const media = window.matchMedia('(max-width: 900px)');
+media.addEventListener('change', (e) => updateNavbar(e));
+
+function updateNavbar(e) {
+    const isMobile = e.matches;
+    console.log(isMobile);
+
+    if (isMobile) {
+        navbar.setAttribute('inert', '');
+    } else {
+        navbar.removeAttribute('inert');
+    }
+}
 
 function openMenu() {
     navbar.classList.add('show');
+    openButton.setAttribute('aria-expanded', 'true');
+    navbar.removeAttribute('inert');
 }
 
 function closeMenu() {
     navbar.classList.remove('show');
+    openButton.setAttribute('aria-expanded', 'false');
+    navbar.setAttribute('inert', '');
 }
+
+updateNavbar(media);
